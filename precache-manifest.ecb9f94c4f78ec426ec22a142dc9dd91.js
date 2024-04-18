d8a5610398ec0e20aab957a599d8e74d1a01da28
Parth Chauhan <pchauhan@originutility.com>
2024-04-18 13:54:56 +0530

    Updates

---
+++ precache-manifest.ecb9f94c4f78ec426ec22a142dc9dd91.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac3d0cf8c98ce4afb7a998fe07f283c",
+    "url": "/parth-weds-kajal/index.html"
+  },
+  {
+    "revision": "b31f7da9c8fec173678a",
+    "url": "/parth-weds-kajal/static/css/2.b560ee55.chunk.css"
+  },
+  {
+    "revision": "c102f87e6a9d258df4e5",
+    "url": "/parth-weds-kajal/static/css/main.dd0b456a.chunk.css"
+  },
+  {
+    "revision": "b31f7da9c8fec173678a",
+    "url": "/parth-weds-kajal/static/js/2.e42c99b2.chunk.js"
+  },
+  {
+    "revision": "fbdb0d29a572e7cde2c9c9c42d89f6b2",
+    "url": "/parth-weds-kajal/static/js/2.e42c99b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c102f87e6a9d258df4e5",
+    "url": "/parth-weds-kajal/static/js/main.a4987d93.chunk.js"
+  },
+  {
+    "revision": "9a36e153c6cc4b7280de",
+    "url": "/parth-weds-kajal/static/js/runtime-main.9ba1046a.js"
+  },
+  {
+    "revision": "2d3a7b84ca02fa517d2106eb5ef60f02",
+    "url": "/parth-weds-kajal/static/media/1.2d3a7b84.jpg"
+  },
+  {
+    "revision": "51103c12763b1625fe6ca84039af5176",
+    "url": "/parth-weds-kajal/static/media/1.51103c12.jpg"
+  },
+  {
+    "revision": "9fdab9d4c6126c692157c5e3f8021c55",
+    "url": "/parth-weds-kajal/static/media/1.9fdab9d4.jpg"
+  },
+  {
+    "revision": "170dde01e9a22d369c7f212d953396e4",
+    "url": "/parth-weds-kajal/static/media/10.170dde01.jpg"
+  },
+  {
+    "revision": "943fac0843dd5c368a99e788170b4a3e",
+    "url": "/parth-weds-kajal/static/media/11.943fac08.jpg"
+  },
+  {
+    "revision": "f8b7a10a7d72bcfe9701b149db996ad9",
+    "url": "/parth-weds-kajal/static/media/12.f8b7a10a.jpg"
+  },
+  {
+    "revision": "c905da42ecfdbde73679f2e0b322eebc",
+    "url": "/parth-weds-kajal/static/media/13.c905da42.jpg"
+  },
+  {
+    "revision": "071cbfbcf31595b57f7b9ed0bf98c48f",
+    "url": "/parth-weds-kajal/static/media/2.071cbfbc.jpg"
+  },
+  {
+    "revision": "478803f4a6dcae9f96ad6737aeca0f29",
+    "url": "/parth-weds-kajal/static/media/2.478803f4.jpg"
+  },
+  {
+    "revision": "9fdab9d4c6126c692157c5e3f8021c55",
+    "url": "/parth-weds-kajal/static/media/2.9fdab9d4.jpg"
+  },
+  {
+    "revision": "6ba94a5622df5be4b5c3f4a585dac786",
+    "url": "/parth-weds-kajal/static/media/3.6ba94a56.jpg"
+  },
+  {
+    "revision": "a232d3763b51fd1dd62c29b3a40b39cf",
+    "url": "/parth-weds-kajal/static/media/3.a232d376.jpg"
+  },
+  {
+    "revision": "ea930a8d31165941ec330420806b1f68",
+    "url": "/parth-weds-kajal/static/media/3.ea930a8d.jpg"
+  },
+  {
+    "revision": "27633829e0be87c1724e6a8b8fe32fc1",
+    "url": "/parth-weds-kajal/static/media/4.27633829.jpg"
+  },
+  {
+    "revision": "75f5049e0da714fe018ecf81a1d4d61e",
+    "url": "/parth-weds-kajal/static/media/4.75f5049e.jpg"
+  },
+  {
+    "revision": "943fac0843dd5c368a99e788170b4a3e",
+    "url": "/parth-weds-kajal/static/media/4.943fac08.jpg"
+  },
+  {
+    "revision": "071cbfbcf31595b57f7b9ed0bf98c48f",
+    "url": "/parth-weds-kajal/static/media/5.071cbfbc.jpg"
+  },
+  {
+    "revision": "8c804792b65f22cf771ff936997455fe",
+    "url": "/parth-weds-kajal/static/media/5.8c804792.jpg"
+  },
+  {
+    "revision": "cf6c578c410ca1ae7a3c616f53a9f42b",
+    "url": "/parth-weds-kajal/static/media/5.cf6c578c.jpg"
+  },
+  {
+    "revision": "499ee9919f05b935212753020d3ba3d0",
+    "url": "/parth-weds-kajal/static/media/6.499ee991.jpg"
+  },
+  {
+    "revision": "f8b10a6eae9cb2a15f0653480876e1da",
+    "url": "/parth-weds-kajal/static/media/6.f8b10a6e.jpg"
+  },
+  {
+    "revision": "4fb3dfad5616c5cd76868761ffd439ae",
+    "url": "/parth-weds-kajal/static/media/7.4fb3dfad.jpg"
+  },
+  {
+    "revision": "6f854fa22512c19211769ced27f24695",
+    "url": "/parth-weds-kajal/static/media/7.6f854fa2.jpg"
+  },
+  {
+    "revision": "d4f4ee0cd0460796585d19d3b190c479",
+    "url": "/parth-weds-kajal/static/media/8.d4f4ee0c.jpg"
+  },
+  {
+    "revision": "38dbeb092244cb81fc1d4cb7bfaf3f5e",
+    "url": "/parth-weds-kajal/static/media/9.38dbeb09.jpg"
+  },
+  {
+    "revision": "ca6f91c0cad2fe33614026d17117601d",
+    "url": "/parth-weds-kajal/static/media/EdwardianScript.ca6f91c0.TTF"
+  },
+  {
+    "revision": "44e86cf59b2b97f7a426d5c0cf45ecd5",
+    "url": "/parth-weds-kajal/static/media/Mandala.44e86cf5.png"
+  },
+  {
+    "revision": "d619369c8e52301e9cae32d40128ea8e",
+    "url": "/parth-weds-kajal/static/media/MandalaTextFG.d619369c.png"
+  },
+  {
+    "revision": "17693b0767f3887a1c6833d7077cd454",
+    "url": "/parth-weds-kajal/static/media/Precious.17693b07.ttf"
+  },
+  {
+    "revision": "9b49a3c6700c268fa548b18ce9e5e268",
+    "url": "/parth-weds-kajal/static/media/bgMain.9b49a3c6.jpg"
+  },
+  {
+    "revision": "a024f4fa0c73953fc77175d2fe1e1e2f",
+    "url": "/parth-weds-kajal/static/media/bgVelvet.a024f4fa.jpg"
+  },
+  {
+    "revision": "bd5ba2e0edc908916aceded2c6db68ab",
+    "url": "/parth-weds-kajal/static/media/bgVertical.bd5ba2e0.jpg"
+  }
+]);
